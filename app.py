a26f9b91a20a6671683f2eb05b7842dc5fa37c23
Goraved <roman.pobotin@gmail.com>
2020-04-29 10:34:22 +0300

    Update webhook

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import logging
 import os
-from flask import Flask, request
+
 import telebot
+from flask import Flask
 
 options = {'Statistics': 'Statistics by today', 'URL': 'Site URL'}
-bot = telebot.TeleBot(os.getenv('TOKEN'))
+TOKEN = os.getenv('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+server = Flask(__name__)
 
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
 keyboard1.row(options['Statistics'], options['URL'])
@@ -25,24 +27,12 @@ def send_text(message):
         bot.send_message(message.chat.id, 'Sorry, I did not understand this command')
 
 
-# Проверим, есть ли переменная окружения Хероку (как ее добавить смотрите ниже)
-if "HEROKU" in list(os.environ.keys()):
-    logger = telebot.logger
-    telebot.logger.setLevel(logging.INFO)
-
-    server = Flask(__name__)
-    @server.route("/bot", methods=['POST'])
-    def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-        return "!", 200
-    @server.route("/")
-    def webhook():
-        bot.remove_webhook()
-        bot.set_webhook(url="https://qa-skills-bot.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
-        return "?", 200
-    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
-else:
-    # если переменной окружения HEROKU нету, значит это запуск с машины разработчика.
-    # Удаляем вебхук на всякий случай, и запускаем с обычным поллингом.
+@server.route("/")
+def webhook():
     bot.remove_webhook()
-    bot.polling(none_stop=True)
+    bot.set_webhook(url="https://qa-skills-bot.herokuapp.com/" + TOKEN)
+    return "!", 200
+
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
