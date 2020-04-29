cfbb213a1849d85606099933249523ee01f8dbab
Goraved <roman.pobotin@gmail.com>
2020-04-29 10:50:36 +0300

    Update webhook 3

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import socket
 
 import telebot
 from flask import Flask, request
@@ -40,4 +41,4 @@ def webhook():
 
 
 if __name__ == '__main__':
-    server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    server.run(host=socket.gethostname(), port=int(os.environ.get('PORT', 5000)))
